ba295a63150670b727b873660f86ca239e59bb3e
BlisS <contacto@fixter.org>
2017-04-08 14:01:55 -0500

    track js ready

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,31 +1,35 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
-
+// cache busting
+import WebpackMd5Hash from 'webpack-md5-hash';
+// css extract
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
   entry: {
-    vendor: path.resolve(__dirname, 'src/vendor'),
-    main: path.resolve(__dirname, 'src/index')
+    main: path.resolve(__dirname, 'src/index'),
+    vendor: path.resolve(__dirname, 'src/vendor')
+
 },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: '[name].[hash].js'
   },
   module: {
     loaders: [
       {
         test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader']
       },
-      {
-        // test: /\.css$/, loaders: ['style-loader','css-loader?modules&localIdentName=[local]---[hash:base64:5]']
-        test: /\.css$/, loaders: ['style-loader','css-loader']
-      },
+      // {
+      //   // test: /\.css$/, loaders: ['style-loader','css-loader?modules&localIdentName=[local]---[hash:base64:5]']
+      //   test: /\.css$/, loaders: ['style-loader','css-loader']
+      // },
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
@@ -33,6 +37,10 @@ export default {
     ]
   },
   plugins: [
+    //css extractor
+    new ExtractTextPlugin('[name].[hash].css'),
+    //ache busting
+    new WebpackMd5Hash(),
     // use common chunk plugin to create a separate bundle
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
@@ -52,7 +60,9 @@ export default {
         minifyCSS: true,
         minifyURLs: true
       },
-      inject: true
+      inject: true,
+      // injectamos track js para produccion
+      trackJSToken: '87beb377dfb84cb19d40ec85704d4f9b'
     }),
     // eliminate duplicate packages
     new webpack.optimize.DedupePlugin(),
